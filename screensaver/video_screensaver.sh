539a5f426018e789718268de44b59c925c4c0764
Heikki Ma <heikki.ma@hotmail.com>
2017-12-06 10:54:20 +0000

    Xscreensaver korjauksia

--- screensaver/video_screensaver.sh
+++ screensaver/video_screensaver.sh
@@ -4,7 +4,7 @@ process() {
 while read input; do 
   case "$input" in
     UNBLANK*)	killall omxplayer.bin ;; # This will kill the process of player
-    BLANK*)	omxplayer --loop --no-osd --no-keys /home/pi/kuvastin/screensaver/720p.mp4 & ;; # dont forget the & on the end or it will never stops, just add your movie file path
+    BLANK*)	omxplayer --loop --no-osd --no-keys --win "0 0 1080 1920" /home/pi/kuvastin/screensaver/720p.mp4 & ;; # dont forget the & on the end or it will never stops, just add your movie file path
   esac
 done
 }
