1cd6c3cfb036cac1aa217126da80568c69f82d2e
Heikki Ma <heikki.ma@hotmail.com>
2017-12-08 13:14:08 +0000

    Näytön kääntö lisätty asennukseen

--- asennus.sh
+++ asennus.sh
@@ -12,4 +12,8 @@ cp ~/kuvastin/snipe.PNG ~/kuvastin/modules/MMM-ImagesPhotos/uploads
 sudo apt-get install -y xscreensaver omxplayer
 sudo chmod +x /home/pi/kuvastin/screensaver/video_screensaver.sh
 cp /home/pi/kuvastin/screensaver/autostart ~/.config/lxsession/LXDE-pi/autostart
+
+sudo mv /boot/config.txt /boot/config.txt.backup
+sudo cp /home/pi/kuvastin/screenrotation/config.txt /boot/config.txt
+
 sudo reboot
